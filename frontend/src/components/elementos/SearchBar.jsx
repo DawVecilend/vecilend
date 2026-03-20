29ddc4ab824094feeea9fb51b57ed1d16843ff2b
frangarciaharo <frangarcia.nuria@gmail.com>
2026-03-20 17:46:56 +0100

    Header en progreso

--- frontend/src/components/elementos/SearchBar.jsx
+++ frontend/src/components/elementos/SearchBar.jsx
@@ -1,7 +1,15 @@
-
 function SearchBar() {
   return (
-    <div>SearchBar</div>
+    <div className="flex">
+      <input
+        className="w-[433px] h-[45px] bg-[#14B8A6] rounded-l-lg border-none px-3 text-white placeholder-white"
+        type="text"
+        placeholder="Busca lo que quieres alquilar"
+      />
+      <button className="w-[82px] h-[45px] bg-[#0F766E] rounded-r-lg text-white">
+        Buscar
+      </button>
+    </div>
   )
 }
 
--- frontend/src/components/header/HeaderDesktop.jsx
+++ frontend/src/components/header/HeaderDesktop.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom'
 import LogoDark from '/assets/logos/LogoDark.svg'
+import SearchBar from '../elementos/SearchBar'
 
 function HeaderDesktop() {
   return (
-    <div className="bg-[#071A16] h-[82px] flex align-center">
-      <Link to="/">
-        <img src={LogoDark} alt="Logo modo oscuro" className="h-[45px] w-[136px]"/>
-      </Link>
-    </div>
+    <header className="bg-[#071A16] h-[82px] flex items-center gap-[20px]">
+        <Link to="/" className="flex items-center">
+          <img src={LogoDark} alt="Logo modo oscuro" className="h-[45px] w-[136px]"/>
+        </Link>
+        <SearchBar/>
+    </header>
   )
 }
 
