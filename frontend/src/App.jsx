a07b4822c526d89dc4b327d91d24deb38e4ce168
frangarciaharo <frangarcia.nuria@gmail.com>
2026-03-27 13:31:49 +0100

    Register hecho

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import StatusPage from './pages/StatusPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/login" element={<LoginPage />}/>
+        <Route path="/register" element={<RegisterPage />}/>
         <Route path="/status" element={<StatusPage />}/>
       </Routes>
     </>
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -26,7 +26,7 @@ function LoginPage() {
         if (!response.ok) {
             const errorText = await response.text();
             console.error("Error del backend:", errorText);
-            throw new Error("Error al i")
+            throw new Error("Error al enviar los datos")
         }
 
         alert("inicio de sesion correcto")
@@ -42,12 +42,12 @@ function LoginPage() {
         <HeaderDesktop />
         <form  onSubmit={handleSubmit} className="relative h-full bg-[url('/assets/fondo-login-register.jpg')] flex flex-col items-center bg-cover bg-center">
             <div className="absolute inset-0 bg-black/90"></div>
-            <div className="relative flex flex-col items-center pt-20 gap-10 w-full px-38 pb-38">
+            <div className="relative flex flex-col items-center pt-20 gap-6 w-full px-38 pb-36">
                 <div className='flex'>
-                    <a href="/register" className='bg-[#16181C] text-white h-full px-8 py-4 rounded-l-2xl'>Crear cuenta</a>
-                    <a href="/login" className='bg-[#14B8A6] h-full px-8 py-4 rounded-r-2xl'>Iniciar sessión</a>
+                    <Link to="/register" className='bg-[#16181C] text-white h-full px-8 py-4 rounded-l-2xl'>Crear cuenta</Link >
+                    <Link to="/login" className='bg-[#14B8A6] h-full px-8 py-4 rounded-r-2xl'>Iniciar sessión</Link >
                 </div>
-                <div className='flex flex-col w-[382px] gap-6'>
+                <div className='flex flex-col w-[382px] gap-[10px]'>
                     <div className='flex items-center'>
                         <img className='relative h-[24px] left-8' src="/assets/icons/mail-icon.svg" alt="Icono email" />
                        <input type="text" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} className='bg-[#4B5563]/40 text-[#D9D9D9] w-full px-10 h-[45px] focus:outline-none rounded-xl' placeholder='Email'/>
@@ -59,14 +59,14 @@ function LoginPage() {
                 </div>
                 <div className='flex items-center flex-col w-full gap-6'>
                     <Link to="#" className='text-[#4B5563]'>¿Olvidaste tu contraseña?</Link>
-                    <button className='bg-[#14B8A6] text-white h-[44px] px-4 flex items-center justify-center rounded-2xl w-[298px]' type="submit">Entrar</button>
+                    <button className='bg-[#14B8A6] text-white h-[44px] px-4 flex items-center justify-center rounded-2xl w-[298px] cursor-pointer' type="submit">Entrar</button>
                 </div>
                 <div className='flex items-center'>
                     <p className='text-[#4B5563] text-center'>Al continuar aceptas nuestros Términos y  la Política de privacidad.</p>
                 </div>
-                <div className='flex flex-col justify-center mt-38'>
+                <div className='flex flex-col justify-center mt-60'>
                     <p className='text-[#4B5563]'>¿No tienes cuenta?</p>
-                    <Link to="" className='text-vecilend-dark-primary text-center w-full'>Crear una aquí</Link>
+                    <Link to="/register" className='text-vecilend-dark-primary text-center w-full'>Crear una aquí</Link>
                 </div>
             </div>
         </form>
