16d99432ff3676565aa785623d4502709ccb9857
ivancarrasco15 <icarrascocnuria@gmail.com>
2026-04-07 17:42:26 +0200

    fix: annoder adjust frontend after merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage';
-import StatusPage from './pages/StatusPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage'
+import StatusPage from './pages/StatusPage'
+import LoginPage from './pages/LoginPage'
+import CategoryPage from './pages/CategoryPage'
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/register" element={<RegisterPage />}/>
-        <Route path="/status" element={<StatusPage />}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/status" element={<StatusPage />} />
+        <Route path="/categorias/:slug" element={<CategoryPage />} />
       </Routes>
     </>
-   
   )
 }
 
