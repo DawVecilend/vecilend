0c94673a0f77e8344f026ef6fb03ad73630ffc6f
frangarciaharo <frangarcia.nuria@gmail.com>
2026-04-07 17:53:29 +0200

    Contexto + axios aplicado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,22 @@ import HomePage from './pages/HomePage';
 import StatusPage from './pages/StatusPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/register" element={<RegisterPage />}/>
-        <Route path="/status" element={<StatusPage />}/>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/status" element={<StatusPage />} />
+        </Routes>
+      </AuthProvider>
     </>
-   
+
   )
 }
 
--- frontend/src/components/header/HeaderDesktop.jsx
+++ frontend/src/components/header/HeaderDesktop.jsx
@@ -3,18 +3,30 @@ import LogoDark from '/assets/logos/LogoDark.svg'
 import SearchBar from '../elementos/SearchBar'
 import PrimaryButton from '../elementos/PrimaryButton'
 import SecondaryButton from '../elementos/SecondaryButton'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function HeaderDesktop() {
+  const { user, logout } = useContext(AuthContext);
   return (
     <header className="bg-vecilend-dark-bg-secondary h-[82px] flex items-center justify-between px-38">
-        <Link to="/" className="flex items-center">
-          <img src={LogoDark} alt="Logo modo oscuro" className="h-[45px] w-[136px]"/>
-        </Link>
-        <SearchBar/>
-        <div className='flex gap-4'>
-          <PrimaryButton url="/login" text="Iniciar sessión"/>
-          <SecondaryButton url="/publication-product" text="Publicar Producto" />
-        </div>
+      <Link to="/" className="flex items-center">
+        <img src={LogoDark} alt="Logo modo oscuro" className="h-[45px] w-[136px]" />
+      </Link>
+      <SearchBar />
+      <div className='flex gap-4'>
+        {user ? (
+          <>
+            <span className="text-white">Hola, {user.name}</span>
+            <PrimaryButton onClick={logout} text="Cerrar sesión" />
+          </>
+        ) : (
+          <>
+            <PrimaryButton url="/login" text="Iniciar sesión" />
+            <SecondaryButton url="/publication-product" text="Publicar Producto" />
+          </>
+        )}
+      </div>
     </header>
   )
 }
