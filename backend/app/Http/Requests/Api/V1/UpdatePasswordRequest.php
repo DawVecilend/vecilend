a6f5caa1736a46bebe29593e410005967f449c83
Eric Hidalgo <eric.hidalgo.vazquez@gmail.com>
2026-04-24 17:50:16 +0200

    Confirmación de distinta contraseña al resetearla

--- backend/app/Http/Requests/Api/V1/UpdatePasswordRequest.php
+++ backend/app/Http/Requests/Api/V1/UpdatePasswordRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests\Api\V1;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class UpdatePasswordRequest extends FormRequest {
     public function authorize(): bool {
@@ -22,7 +23,13 @@ class UpdatePasswordRequest extends FormRequest {
                     }
                 }
             ],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => [
+                'required',
+                Password::min(8)->letters()->mixedCase()->numbers()->symbols(),
+                'string',
+                'confirmed',
+                'different:current_password'
+            ],
         ];
     }
 
@@ -32,7 +39,14 @@ class UpdatePasswordRequest extends FormRequest {
             'current_password.current_password' => 'La contraseña actual no es correcta.',
             'password.required' => 'La nueva contraseña es obligatoria.',
             'password.min' => 'La nueva contraseña debe tener al menos 8 caracteres.',
+            'password.letters' => 'La nueva contraseña debe contener letras.',
+            'password.mixedCase' => 'La nueva contraseña debe contener letras mayúsculas y minúsculas.',
+            'password.numbers' => 'La nueva contraseña debe contener números.',
+            'password.symbols' => 'La nueva contraseña debe contener símbolos.',
             'password.confirmed' => 'Las contraseñas no coinciden.',
+            'password.different' => 'La nueva contraseña tiene que ser distinta a la actual.',
+
+
         ];
     }
 }
